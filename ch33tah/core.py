5222389b2954ce8c16b058c0d58487cd233c96df
jackhwolf <jwolf22@wisc.edu>
2020-03-02 01:45:40 -0600

    added predictions

--- ch33tah/core.py
+++ ch33tah/core.py
@@ -1,25 +1,24 @@
-from experiment import Experiment
-from dataset import Dataset
 import numpy as np
-import json
-from distributed import Client
-import time
-import joblib
 import os
-from resultsmanager import ResultsManager
+import joblib
 import shutil 
-from model import Model 
+import time
+from distributed import Client
 from sklearn.metrics import accuracy_score, mean_squared_error
+from ch33tah.experiment import Experiment
+from ch33tah.dataset import Dataset
+from ch33tah.resultsmanager import ResultsManager
+from ch33tah.model import Model 
 
 np.random.seed(1)
 
 class Ch33tah:
+    ''' main class for this project. How the user will interact with Ch33tah core. '''
 
     def __init__(self, data, label, task, name, **kw):
         ''' get ready to run '''
         self.experiment = Experiment(Dataset(data, label), task)
         self.name = name
-        os.makedirs(self.name, exist_ok=True)
         self.resmngr = ResultsManager()
         self.bucket = None
 
@@ -34,19 +33,20 @@ class Ch33tah:
     def save_models(self, res):
         ''' save the results of the good models. we are going to dump the weigths 
         to an S3 bucket of the user '''
-        bucket_name = self.resmngr.create_bucket(self.name)
-        self.bucket = bucket_name
+        self.bucket = self.resmngr.create_bucket(self.name.lower()+"-"+self.experiment.task.lower())
+        os.makedirs(self.name, exist_ok=True)
         for mdl in list(res):
             tosave = res[mdl]['model']
             fname = f"{self.name}/{mdl}.sav"
             joblib.dump(tosave, fname)
-        self.resmngr.upload_run(bucket_name, self.name)
+        self.resmngr.upload_run(self.bucket, self.name)
         shutil.rmtree(self.name)
+        return self.bucket
 
     def load_models(self, bucket=None):
         ''' load some models in from a previous run for inference '''
         bucket = self.bucket if bucket is None else bucket
-        models = self.resmngr.reload_models(bucket)
+        models = self.resmngr.reload_models(bucket.lower())
         self.models = models
         return self.models
 
@@ -71,58 +71,45 @@ class Ch33tah:
             preds[name] = {'predictions': y_hat, 'performance': perf}
         return preds
 
+    
 class Ch33tahRetest:
+    ''' class to reload previous models and use them for prediction '''
 
-    def __init__(self, bucket, task):
+    def __init__(self, bucket, load=0):
         ''' point me towards the s3 bucket holding your models '''
         self.bucket = bucket
         self.models = None
         self.resmngr = ResultsManager()
-        self.task = task
-        self.load_models()
+        self.task = bucket.split('-')[1]
+        if load:
+            self.load_models()
 
     def load_models(self):
         ''' same as above '''
-        models = self.resmngr.reload_models(self.bucket)
-        self.models = models
-        return self.models
+        self.models = self.resmngr.reload_models(self.bucket)
+        return self.models.copy()
 
-    def evaluate_test_set(self, X, y, m='all'):
+    def evaluate_test_set(self, dataset, m='all'):
         ''' same as above again '''
-        preds = []
+        preds = self.predict(dataset.x, m=m)
+        y = dataset.y
+        for p in preds:
+            y_hat = preds[p]['predictions']
+            if self.task == 'classification':
+                perf = {'accuracy': accuracy_score(y, y_hat)}
+            else:
+                perf = {'mean_squared_err': mean_squared_error(y, y_hat)}
+            preds[p].update(**{'performance': perf})
+        return preds
+
+    def predict(self, X, m='all'):
         if m != 'all':
-            if not isinstance(m, list):
-                m = [m]
+            m = [m] if not isinstance(m, list) else m
         else:
             m = list(self.models)
         preds = {}
-        for name, model in models.items():
+        for name, model in self.models.items():
             model = Model((model, None), self.task)
             y_hat = model.mdl.predict(X)
-            perf = model.get_performance_metric(y, y_hat)
-            if self.task == 'classification':
-                perf = {'accuracy': perf}
-            else:
-                perf = {'mean_squared_err': perf}
-            preds[name] = {'predictions': y_hat, 'performance': perf}
+            preds[name] = {'predictions': y_hat}
         return preds
-
-if __name__ == '__main__':
-    X = np.round(np.random.randn(10000,5), 3)
-    y = (X[:,[0]] > 0).astype(int)
-    X = np.hstack((X, y))
-    
-    data = Dataset(X, 5)
-    core = Ch33tah(X, 5, 'classification', 'test')
-    results = core.ch33t()
-    core.save_models(results)
-
-    models = core.load_models()
-
-    X2 = np.round(np.random.randn(10, 5), 3)
-    y2 = (X2[:, [0]] > 0).astype(int)
-
-    core_retest = Ch33tahRetest(core.bucket, 'classification')
-
-    print(core_retest.evaluate_test_set(X2, y2))
-
